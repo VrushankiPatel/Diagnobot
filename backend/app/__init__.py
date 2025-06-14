cda4dd7729f0f5438589e6afcab94b1b372449cf
vruspate <vrushanki.jigarkumar.patel@oracle.com>
2025-06-14 10:48:12 -0700

    DIABOT-01-bcknd-VRUSH GCP configuration

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -11,4 +11,4 @@ app.include_router(admin.router, prefix="/admin")
 app.include_router(chatroom.router, prefix="/chat")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
